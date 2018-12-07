b5e5c1370d2929cbd5c3ac95cddddc6678a4b2ff
janvandinther <34287804+janvandinther@users.noreply.github.com>
2018-12-07 16:34:43 +0100

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,12 +1,28 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from forms import LoginForm, RegisterForm
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'This is supposed to be secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
 Bootstrap(app)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
+from forms import LoginForm, RegisterForm
+from database import User
+
+
+@app.before_first_request
+def setup_db():
+    db.drop_all()
+    db.create_all()
 
 @app.route('/')
 @app.route('/index')
@@ -16,21 +32,48 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
+        else:
+            flash('You are not logged in, please try again.', 'danger')
     return render_template('login.html', form = form)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in...', 'success')
+        return redirect(url_for('login'))
     return render_template('signup.html', form = form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/studyrooms')
 def studyrooms():
     return render_template('table.html')
 
-
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html',title='Account')
 
 if __name__ == '_main_':
     app.run()
+
--- forms.py
+++ forms.py
@@ -1,15 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-
+from wtforms.validators import InputRequired, Email, Length, EqualTo, ValidationError
+from database import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username: ', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('Password: ', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('Remember me')
 
-
 class RegisterForm(FlaskForm):
     username = StringField('Username:', validators=[InputRequired(), Length(min=4, max=15)])
     email = StringField('Email: ', validators=[InputRequired(), Email(), Length(max=50)])
     password = PasswordField('Password: ', validators=[InputRequired(), Length(min=8, max=80)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken. Take another one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already taken. Take another one')
+
